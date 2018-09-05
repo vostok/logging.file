4a40165aa4e34776ac77d75afeaa9cfa86b73c4a
iloktionov <iloktionov@skbkontur.ru>
2018-09-05 20:39:23 +0500

    IEventsWriterProvider: quick xml-docs.

--- Vostok.Logging.File/EventsWriting/IEventsWriterProvider.cs
+++ Vostok.Logging.File/EventsWriting/IEventsWriterProvider.cs
@@ -6,6 +6,11 @@ namespace Vostok.Logging.File.EventsWriting
 {
     internal interface IEventsWriterProvider : IDisposable
     {
+        /// <summary>
+        /// <para>Attempts to obtain an <see cref="IEventsWriter"/> instance to write <see cref="LogEventInfo"/>s with.</para>
+        /// <para>Returns <c>null</c> on any failure, such as file opening error.</para>
+        /// <para>May wait for a cooldown provided by <see cref="ICooldownController"/> before trying to open a writer.</para>
+        /// </summary>
         [ItemCanBeNull]
         Task<IEventsWriter> ObtainWriterAsync();
     }
