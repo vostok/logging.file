b350f1b5b372d5e3c2b5f8a6f24eed0c677e1545
Krait <krait@skbkontur.ru>
2018-09-05 18:11:14 +0500

    Added more xml-docs; some minor fixes.

--- Vostok.Logging.File/Configuration/RollingStrategyType.cs
+++ Vostok.Logging.File/Configuration/RollingStrategyType.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using JetBrains.Annotations;
 
-#pragma warning disable 1591
-
 namespace Vostok.Logging.File.Configuration
 {
+    /// <summary>
+    /// Specifies the rolling behavior: when to switch to the next part of log file.
+    /// </summary>
     [PublicAPI]
     [Flags]
     public enum RollingStrategyType
     {
+        /// <summary>
+        /// No rolling, just always write to one file.
+        /// </summary>
         None = 0b00,
+
+        /// <summary>
+        /// Switch to next file once in a specified time period.
+        /// </summary>
         ByTime = 0b01,
+
+        /// <summary>
+        /// Switch to next file when current file reaches specified size.
+        /// </summary>
         BySize = 0b10,
+
+        /// <summary>
+        /// A combination of <see cref="ByTime"/> and <see cref="BySize"/> approaches. Will switch to next file as soon as any of the conditions is met.
+        /// </summary>
         Hybrid = 0b11
     }
 }
