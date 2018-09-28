6d445fa03c12ff45c53c134f439af887ee69f3f8
alexz <alexz@skbkontur.ru>
2018-09-28 14:12:37 +0500

    Refactored functional tests.

---
+++ Vostok.Logging.File.Tests/Functional/FileLogFunctionalTestsBase.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.File.Helpers;
+
+namespace Vostok.Logging.File.Tests.Functional
+{
+    internal class FileLogFunctionalTestsBase
+    {
+        protected TemporaryFolder Folder;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            Folder = new TemporaryFolder();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Folder.Dispose();
+        }
+        
+        protected static string FormatMessage(int index) =>
+            $"Message #{index}";
+
+        protected static string[] GenerateMessages(int startIndex, int endIndex)
+        {
+            return Enumerable
+                .Range(startIndex, endIndex - startIndex)
+                .Select(FormatMessage)
+                .ToArray();
+        }
+
+        protected static void WriteMessagesWithTimeout(FileLog log, IEnumerable<string> messages, int sleepTimeout = 10)
+        {
+            foreach (var message in messages)
+            {
+                log.Info(message);
+                Thread.Sleep(sleepTimeout);
+            }
+        }
+
+        protected static void ShouldContainMessages(FilePath fileName, IEnumerable<string> messages)
+        {
+            var text = System.IO.File.ReadAllText(fileName.NormalizedPath);
+
+            foreach (var message in messages)
+            {
+                text.Should().Contain(message);
+            }
+        }
+        
+        protected static void ShouldContainMessage(FilePath fileName, string message)
+        {
+            ShouldContainMessages(fileName, new []{message});
+        }
+    }
+}
