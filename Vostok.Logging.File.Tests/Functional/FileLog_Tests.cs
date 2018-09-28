3a3eb47a79096a8f2c01ddbf55be9f963a9614c6
alexz <alexz@skbkontur.ru>
2018-09-28 18:58:14 +0500

    Added functional tests for log levels and FileOpenMode setting.

--- Vostok.Logging.File.Tests/Functional/FileLog_Tests.cs
+++ Vostok.Logging.File.Tests/Functional/FileLog_Tests.cs
@@ -77,6 +77,71 @@ namespace Vostok.Logging.File.Tests.Functional
         }
         
         [Test]
+        public void Should_write_events_with_enabled_levels_only()
+        {
+            var logName = Folder.GetFileName("log");
+
+            using (var log = new FileLog(new FileLogSettings
+            {
+                FilePath = logName,
+                EnabledLogLevels = new []{LogLevel.Error, LogLevel.Fatal}
+            }))
+            {
+                log.Fatal("Fatal");
+                log.Error("Error");
+                log.Info("Info");
+            }
+
+            System.IO.File.Exists(logName).Should().BeTrue();
+
+            var logText = System.IO.File.ReadAllText(logName);
+            logText.Should().ContainAll("Fatal", "Error");
+            logText.Should().NotContain("Info");
+        }
+        
+        [Test]
+        public void Should_append_to_file_by_default()
+        {
+            var logName = Folder.GetFileName("log");
+
+            using (var log = new FileLog(new FileLogSettings {FilePath = logName}))
+            {
+                log.Info(FormatMessage(0));
+            }
+            
+            using (var log = new FileLog(new FileLogSettings { FilePath = logName }))
+            {
+                log.Info(FormatMessage(1));
+            }
+
+            System.IO.File.Exists(logName).Should().BeTrue();
+            ShouldContainMessages(logName, new []{ FormatMessage(0), FormatMessage(1) });
+        }
+        
+        [Test]
+        public void Should_rewrite_file_when_configured()
+        {
+            var logName = Folder.GetFileName("log");
+
+            using (var log = new FileLog(new FileLogSettings {FilePath = logName}))
+            {
+                log.Info(FormatMessage(0));
+            }
+
+            System.IO.File.Exists(logName).Should().BeTrue();
+            ShouldContainMessage(logName, FormatMessage(0));
+            
+            using (var log = new FileLog(new FileLogSettings { FilePath = logName, FileOpenMode = FileOpenMode.Rewrite }))
+            {
+                log.Info(FormatMessage(1));
+            }
+
+            var logText = System.IO.File.ReadAllText(logName);
+            logText.Should().NotContain(FormatMessage(0));
+            logText.Should().Contain(FormatMessage(1));
+        }
+        
+        [Test]
         public void Should_create_directories_leading_to_log_file_path_if_needed()
         {
             var logName = Path.Combine(Folder.Name, "dir1", "dir2", "log");
