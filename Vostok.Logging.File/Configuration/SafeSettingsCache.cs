ecb0e6a9facbabea062de6f21ebcd511570e5d3a
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-05-07 11:35:06 +0500

    + SafeSettingsCache

---
+++ Vostok.Logging.File/Configuration/SafeSettingsCache.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vostok.Logging.File.Configuration
+{
+    internal class SafeSettingsCache
+    {
+        private readonly SafeSettingsProvider provider;
+        private readonly TimeSpan ttl = TimeSpan.FromSeconds(1);
+        private volatile Lazy<FileLogSettings> container;
+
+        public SafeSettingsCache(Func<FileLogSettings> provider)
+        {
+            this.provider = new SafeSettingsProvider(provider);
+
+            ResetContainer();
+        }
+
+        public SafeSettingsCache(Func<FileLogSettings> provider, TimeSpan ttl)
+        {
+            this.provider = new SafeSettingsProvider(provider);
+            this.ttl = ttl;
+
+            ResetContainer();
+        }
+
+        public FileLogSettings Get() => container.Value;
+
+        private void ResetContainer()
+        {
+            container = new Lazy<FileLogSettings>(
+                () =>
+                {
+                    try
+                    {
+                        return provider.Get();
+                    }
+                    finally
+                    {
+                        Task.Delay(ttl).ContinueWith(_ => ResetContainer());
+                    }
+                },
+                LazyThreadSafetyMode.ExecutionAndPublication);
+        }
+    }
+}
