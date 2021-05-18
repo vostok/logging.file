86285a5889ab3ff2c309b20d0596630dabf056a9
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-05-18 12:18:57 +0500

    Lazy -> AsyncUpdate

--- Vostok.Logging.File/Configuration/SafeSettingsCache.cs
+++ Vostok.Logging.File/Configuration/SafeSettingsCache.cs
@@ -6,40 +6,41 @@ namespace Vostok.Logging.File.Configuration
 {
     internal class SafeSettingsCache
     {
+        private static readonly object CooldownGuard = new object();
+
         private readonly SafeSettingsProvider provider;
         private readonly TimeSpan ttl = TimeSpan.FromSeconds(1);
         private readonly bool enabled;
-        private volatile Lazy<FileLogSettings> container;
+
+        private volatile object updateCooldown;
+        private volatile FileLogSettings currentSettings;
 
         public SafeSettingsCache(Func<FileLogSettings> provider)
         {
             this.provider = new SafeSettingsProvider(provider);
 
-            enabled = this.provider.Get().EnableFileLogSettingsCache;
+            var settings = this.provider.Get();
+
+            enabled = settings.EnableFileLogSettingsCache;
 
             if (enabled)
-                ResetContainer();
+                currentSettings = settings;
         }
 
-        public FileLogSettings Get() => enabled
-            ? container.Value
-            : provider.Get();
-
-        private void ResetContainer()
+        public FileLogSettings Get()
         {
-            container = new Lazy<FileLogSettings>(
-                () =>
-                {
-                    try
-                    {
-                        return provider.Get();
-                    }
-                    finally
-                    {
-                        Task.Delay(ttl).ContinueWith(_ => ResetContainer());
-                    }
-                },
-                LazyThreadSafetyMode.ExecutionAndPublication);
+            if (!enabled)
+                return provider.Get();
+
+            if (updateCooldown == null && Interlocked.CompareExchange(ref updateCooldown, CooldownGuard, null) == null)
+            {
+                Task.Run(() => currentSettings = provider.Get())
+                   .ContinueWith(_ => Task.Delay(ttl))
+                   .Unwrap()
+                   .ContinueWith(_ => updateCooldown = null);
+            }
+
+            return currentSettings;
         }
     }
 }
