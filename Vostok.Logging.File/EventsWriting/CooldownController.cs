ece84f5acb4bd6d5257456f620aadee721c84501
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-06-08 10:31:53 +0500

    Added drop cooldown to EventsWriterProvider

--- Vostok.Logging.File/EventsWriting/CooldownController.cs
+++ Vostok.Logging.File/EventsWriting/CooldownController.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Signal = System.Threading.Tasks.TaskCompletionSource<bool>;
 
 namespace Vostok.Logging.File.EventsWriting
 {
     internal class CooldownController : ICooldownController
     {
+        private Signal endImmediately = new Signal();
         private Task cooldownTask = Task.CompletedTask;
 
         public bool IsCool => cooldownTask.IsCompleted;
 
-        public Task WaitForCooldownAsync() => cooldownTask;
+        public Task WaitForCooldownAsync() => Task.WhenAny(cooldownTask, endImmediately.Task);
 
-        public void IncurCooldown(TimeSpan duration, CancellationToken cancellation) =>
+        public void IncurCooldown(TimeSpan duration, CancellationToken cancellation)
+        {
+            endImmediately = new Signal();
             cooldownTask = Task.Delay(duration, cancellation);
+        }
+
+        public void DropCooldown()
+        {
+            endImmediately.SetResult(true);
+        }
     }
 }
