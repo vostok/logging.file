5fbdd571ae374533e92bf636e808744a8b14735b
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-06-08 14:46:04 +0500

    Fixed tests

--- Vostok.Logging.File/EventsWriting/CooldownController.cs
+++ Vostok.Logging.File/EventsWriting/CooldownController.cs
@@ -8,16 +8,16 @@ namespace Vostok.Logging.File.EventsWriting
     internal class CooldownController : ICooldownController
     {
         private readonly Signal endImmediately = new Signal(false);
-        private Task cooldownTask = Task.CompletedTask;
+        private Task cooldownTask = Task.FromResult(Task.CompletedTask);
 
         public bool IsCool => cooldownTask.IsCompleted;
 
-        public Task WaitForCooldownAsync() => Task.WhenAny(cooldownTask, endImmediately);
+        public Task WaitForCooldownAsync() => cooldownTask;
 
         public void IncurCooldown(TimeSpan duration, CancellationToken cancellation)
         {
             endImmediately.Reset();
-            cooldownTask = Task.Delay(duration, cancellation);
+            cooldownTask = Task.WhenAny(Task.Delay(duration, cancellation), endImmediately.WaitAsync(cancellation)).Unwrap();
         }
 
         public void DropCooldown() => endImmediately.Set();
