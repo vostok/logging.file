6924d20eb50e9715a51d1e0b51b80e17eff111e5
kungurtsev <kungurtsev@skbkontur.ru>
2022-07-18 16:24:35 +0500

    drain events as batch

--- Vostok.Logging.File/Muxers/SingleFileMuxer.cs
+++ Vostok.Logging.File/Muxers/SingleFileMuxer.cs
@@ -32,6 +32,7 @@ namespace Vostok.Logging.File.Muxers
     /// </summary>
     internal class SingleFileMuxer : ISingleFileMuxer
     {
+        private static readonly TimeSpan NewEventsDelay = TimeSpan.FromMilliseconds(100);
         private static readonly TimeSpan NewEventsTimeout = TimeSpan.FromSeconds(1);
         private static readonly List<Waiter> EmptyWaitersList = new List<Waiter>();
 
@@ -64,7 +65,7 @@ namespace Vostok.Logging.File.Muxers
             writerProvider = writerProviderFactory.CreateProvider(settings.FilePath, () => this.settings);
 
             eventsQueue = new Lazy<ConcurrentBoundedQueue<LogEventInfo>>(
-                () => new ConcurrentBoundedQueue<LogEventInfo>(settings.EventsQueueCapacity),
+                () => new ConcurrentBoundedQueue<LogEventInfo>(settings.EventsQueueCapacity, Math.Max(1, settings.EventsQueueCapacity / 20)),
                 LazyThreadSafetyMode.ExecutionAndPublication);
 
             eventsBuffer = new Lazy<LogEventInfo[]>(
@@ -211,7 +212,7 @@ namespace Vostok.Logging.File.Muxers
                 }
 
                 await Task.WhenAny(
-                        eventsQueue.Value.TryWaitForNewItemsAsync(NewEventsTimeout),
+                        eventsQueue.Value.TryWaitForNewItemsBatchAsync(NewEventsDelay, NewEventsTimeout),
                         flushSignal.WaitAsync(),
                         workerCancellationWaiter.Task)
                     .ConfigureAwait(false);
